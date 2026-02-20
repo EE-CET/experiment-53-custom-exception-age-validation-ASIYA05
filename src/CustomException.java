dffe88b4ef9a2969cbf96ac53e2780c19aa3f49b
ASIYA05 <tve24el013@cet.ac.in>
2026-02-20 16:19:42 +0530

    Update CustomException.java

--- src/CustomException.java
+++ src/CustomException.java
@@ -6,22 +6,21 @@ class InvalidAgeException extends Exception {
     }
 }
 
-public class Main {
+public class CustomException {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int age = sc.nextInt();
-        
+
         try {
             if (age < 18) {
                 throw new InvalidAgeException("Not eligible to vote");
             } else {
                 System.out.println("Eligible to vote");
             }
-        } 
-        catch (InvalidAgeException e) {
+        } catch (InvalidAgeException e) {
             System.out.println("Exception: " + e.getMessage());
         }
-        
+
         sc.close();
     }
 }
